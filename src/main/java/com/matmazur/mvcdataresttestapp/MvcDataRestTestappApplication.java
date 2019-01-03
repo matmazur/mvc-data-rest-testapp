4bc85234b4716cdfce1b5a19012be291a2cda15e
Bingo Bobo <matmazur90@gmail.com>
2019-01-03 19:47:49 +0100

    initial commit

---
+++ src/main/java/com/matmazur/mvcdataresttestapp/MvcDataRestTestappApplication.java
@@ -0,0 +1,14 @@
+package com.matmazur.mvcdataresttestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcDataRestTestappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcDataRestTestappApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/matmazur/mvcdataresttestapp/MvcDataRestTestappApplicationTests.java
@@ -0,0 +1,17 @@
+package com.matmazur.mvcdataresttestapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MvcDataRestTestappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+

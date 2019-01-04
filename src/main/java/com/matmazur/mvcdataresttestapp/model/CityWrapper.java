4eac4e7d6b87e504ae964b0ec6f5ed13590d58fe
Bingo Bobo <matmazur90@gmail.com>
2019-01-04 15:55:33 +0100

    added CityWrapper in case of xml data producing in a list while using rest api

---
+++ src/main/java/com/matmazur/mvcdataresttestapp/model/CityWrapper.java
@@ -0,0 +1,17 @@
+package com.matmazur.mvcdataresttestapp.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CityWrapper {
+
+    private List<City> list = new ArrayList<>();
+
+    public List<City> getList() {
+        return list;
+    }
+
+    public void setList(List<City> list) {
+        this.list = list;
+    }
+}
--- src/main/java/com/matmazur/mvcdataresttestapp/rest/CityRestController.java
+++ src/main/java/com/matmazur/mvcdataresttestapp/rest/CityRestController.java
@@ -1,7 +1,20 @@
 package com.matmazur.mvcdataresttestapp.rest;
 
+import com.matmazur.mvcdataresttestapp.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/cities")
 public class CityRestController {
+
+    private final
+    CityRepository cityRepository;
+
+    @Autowired
+    public CityRestController(CityRepository repository) {
+        this.cityRepository = repository;
+    }
+
 }
